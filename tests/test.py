565a229b787afc5e286c06ff2e6eca7242a0f4bb
mazunki <rolferen@gmail.com>
2022-03-11 18:06:13 +0100

    added the start for testing

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.10
+
+import unittest
+from smol_sh import utils
+
+class TestArguments(unittest.TestCase):
+    def test_short_arguments(self):
+        sh = SmolShell()
+
+if __name__ == "__main__":
+    unittest.main()
